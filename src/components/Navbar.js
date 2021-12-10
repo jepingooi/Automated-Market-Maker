4dd4f9556e614e0f6035b1a4c78d315dcf54167f
Asus <williamng0512@gmail.com>
2021-12-10 22:32:16 +0800

    edited name to automated market maker

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,14 +5,7 @@ class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-        <a
-          className="navbar-brand col-sm-3 col-md-2 mr-0"
-          href="http://www.dappuniversity.com/bootcamp"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Automated Market Maker
-        </a>
+        <div className="navbar-brand col-sm-3 col-md-2 mr-0">Automated Market Maker</div>
 
         <ul className="navbar-nav px-3">
           <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
@@ -25,10 +18,7 @@ class Navbar extends Component {
                 className="ml-2"
                 width="30"
                 height="30"
-                src={`data:image/png;base64,${new Identicon(
-                  this.props.account,
-                  30
-                ).toString()}`}
+                src={`data:image/png;base64,${new Identicon(this.props.account, 30).toString()}`}
                 alt=""
               />
             ) : (
